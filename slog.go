6d53b94bdde6ecdfc203f3ea03f3c3b68c8383b8
Levi Cook <levicook@gmail.com>
2014-06-18 16:45:59 -0600

    first commit

---
+++ slog.go
@@ -0,0 +1,44 @@
+package slog
+
+import (
+	stdLog "log"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
+var (
+	Fatal   = stdLog.Fatal
+	Fatalf  = stdLog.Fatalf
+	Fatalln = stdLog.Fatalln
+	Panic   = stdLog.Panic
+	Panicf  = stdLog.Panicf
+	Panicln = stdLog.Panicln
+	Prefix  = stdLog.Prefix
+	Print   = stdLog.Print
+	Printf  = stdLog.Printf
+	Println = stdLog.Println
+)
+
+func init() {
+	stdLog.SetFlags(0)
+}
+
+func Dump(v ...interface{}) {
+	spew.Dump(v...)
+}
+
+func FatalIf(err error) {
+	if err != nil {
+		stdLog.Fatal(err)
+	}
+}
+
+func PanicIf(err error) {
+	if err != nil {
+		stdLog.Panic(err)
+	}
+}
+
+func SetPrefix(s string) {
+	stdLog.SetPrefix("[" + s + "] ")
+}
